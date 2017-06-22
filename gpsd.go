65d82a4bc7ae7682993e9d43fa67ae63b61192ce
SmileSteven <zx4567zx4567@gmail.com>
2017-06-22 09:33:44 +0800

    Add a channel to stop data report when it is necessary.

--- gpsd.go
+++ gpsd.go
@@ -239,24 +239,33 @@ func watch(done chan bool, s *Session) {
 	// We're not using a JSON decoder because we first need to inspect
 	// the JSON string to determine it's "class"
 	for {
-		if line, err := s.reader.ReadString('\n'); err == nil {
-			var reportPeek gpsdReport
-			lineBytes := []byte(line)
-			if err = json.Unmarshal(lineBytes, &reportPeek); err == nil {
-				if len(s.filters[reportPeek.Class]) == 0 {
-					continue
-				}
+		select {
+		case status := <-done:
+			if status {
+				fmt.Println("close channel!")
+				done <- true
+				break
+			}
+		default:
+			if line, err := s.reader.ReadString('\n'); err == nil {
+				var reportPeek gpsdReport
+				lineBytes := []byte(line)
+				if err = json.Unmarshal(lineBytes, &reportPeek); err == nil {
+					if len(s.filters[reportPeek.Class]) == 0 {
+						continue
+					}
 
-				if report, err2 := unmarshalReport(reportPeek.Class, lineBytes); err2 == nil {
-					s.deliverReport(reportPeek.Class, report)
+					if report, err2 := unmarshalReport(reportPeek.Class, lineBytes); err2 == nil {
+						s.deliverReport(reportPeek.Class, report)
+					} else {
+						fmt.Println("JSON parsing error 2:", err)
+					}
 				} else {
-					fmt.Println("JSON parsing error 2:", err)
+					fmt.Println("JSON parsing error:", err)
 				}
 			} else {
-				fmt.Println("JSON parsing error:", err)
+				fmt.Println("Stream reader error (is gpsd running?):", err)
 			}
-		} else {
-			fmt.Println("Stream reader error (is gpsd running?):", err)
 		}
 	}
 }
